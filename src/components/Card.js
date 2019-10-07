301ebee34287094e56178c3710990e42e98e08a7
Liam Dawson <liamrdawson@gmail.com>
2019-10-07 20:10:40 +0100

    convert Main.js to class component and add state, then add logic to Card.js to generate Cards based on props

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,11 @@ const Card = (props) => {
     return (
         <div className="card">
             <h3>{props.name}</h3>
+            <ul>
+                { props.topicsCovered.map(topic =>
+                <li>{topic}</li>) 
+                }
+            </ul>
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from './Card';
 
-const Main = () => {
-    return (
-        <React.Fragment>
-        <h2>These are the topics you've covered so far:</h2>
-            <div class="card-container">
-                <Card name="Starting Git" />
-            </div>
-        </React.Fragment>
-    );
+class Main extends Component {
+
+    state = {
+        cards: [
+            {
+                name: "Starting Git",
+                topicsCovered: ["First commit", "Staging changed files"]
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+            <h2>These are the topics you've covered so far:</h2>
+                <div class="card-container">
+                    {this.state.cards.map(infoCard => 
+                        <Card name={infoCard.name} topicsCovered={infoCard.topicsCovered}/>
+                    )}
+                </div>
+            </React.Fragment>
+        );
+    }
+
 }
 
 export default Main;
